191b978b0ade6c8c4ab710ec8040893a5539e2e3
Caleb Spain <crspain@uh.edu>
2019-10-12 19:49:04 -0500

    created demo controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'demo#index'
+  get 'demo/index'
+  # default route
+  # may go out in futer versions of rails
+  # get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
